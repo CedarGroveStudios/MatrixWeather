6ab9a26c413a12f0593c4fb4853b090aa94adcdd
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-10-25 10:53:33 -0700

    update failover; use microcontroller.reset()

--- bundle_8.0.0/matrixportal_failover.py
+++ bundle_8.0.0/matrixportal_failover.py
@@ -8,23 +8,23 @@
 Used after a fatal error to dim the display to keep the board cooler and flash
 the NeoPixel for attention.
 
-matrixportal_failover.py  2022-09-02 v1.0  Cedar Grove Studios
+matrixportal_failover.py  2022-10-25 v1.1  Cedar Grove Studios
 
 * Author(s): JG for Cedar Grove Maker Studios
 """
 
-# imports__version__ = "0.0.0-auto.0"
+# imports__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/CedarGroveStudios/Failover"
 
 
 import board
+import time
+import microcontroller
 import neopixel
-import time
-
-import board
 import digitalio
-import time
 from adafruit_matrixportal.matrix import Matrix
+
+DELAY = 20  # seconds
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
@@ -33,7 +33,12 @@ led.value = True
 matrix = Matrix(bit_depth=1)  # default is 2; maximum is 6
 matrix.display.brightness = 0  # blank display during startup
 
-while True:
-    """Flash red LED forever."""
-    time.sleep(1)
+print("matrixportal_failover: begin reset delay")
+end_delay = time.monotonic() + DELAY
+while time.monotonic() < end_delay:
+    """Flash red LED during delay period."""
+    time.sleep(0.5)
     led.value = not led.value
+
+print("matrixportal_failover: resetting microcontroller")
+microcontroller.reset()
